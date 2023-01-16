2f92c2e2683fafd70f5f44ac6dd7fcf91c8236c0
Filip <filip.uradnik9@gmail.com>
2023-01-16 16:44:16 +0100

    Add option for brightness.

--- yearly_hue/__init__.py
+++ yearly_hue/__init__.py
@@ -9,12 +9,12 @@ def _i_to_h(i: float) -> str:
     return h[-2:]
 
 
-def get_hue(saturation: float = 1.0) -> str:
+def get_hue(brightness: float = 1.0, saturation: float = 1.0) -> str:
     """Return a hex representation of a hue."""
     t = datetime.today()
     hue = (int(t.strftime("%j")) - 1 + (t.hour + (t.minute + t.second / 60) / 60) / 24) / 365
     hue = (6 - hue + 0.6)
     while hue > 1:
         hue -= 1
-    r, g, b = colorsys.hsv_to_rgb(hue, 1, saturation)
+    r, g, b = colorsys.hsv_to_rgb(hue, brightness, saturation)
     return "#" + _i_to_h(r) + _i_to_h(g) + _i_to_h(b)
