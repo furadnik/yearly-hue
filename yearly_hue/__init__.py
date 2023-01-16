8f97040fec2007820324787d1b6d85e9eff799cb
Filip <filip.uradnik9@gmail.com>
2023-01-16 15:48:26 +0100

    Add yearly hue.

---
+++ yearly_hue/__init__.py
@@ -0,0 +1,20 @@
+"""Get hue."""
+from datetime import datetime
+import colorsys
+
+
+def _i_to_h(i: float) -> str:
+    h = hex(int(i * 255))
+    h = "00" + h[2:]
+    return h[-2:]
+
+
+def get_hue(saturation: float = 1.0) -> str:
+    """Return a hex representation of a hue."""
+    t = datetime.today()
+    hue = (int(t.strftime("%j")) - 1 + (t.hour + (t.minute + t.second / 60) / 60) / 24) / 365
+    hue = (6 - hue + 0.6)
+    while hue > 1:
+        hue -= 1
+    r, g, b = colorsys.hsv_to_rgb(hue, 1, saturation)
+    return "#" + _i_to_h(r) + _i_to_h(g) + _i_to_h(b)
