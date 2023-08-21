5844a1619c448c5696c5b3865fe9e4ebfd1103fd
Filip Uradnik <filip.uradnik@nic.cz>
2023-08-21 11:00:22 +0200

    Add format support.

--- yearly_hue/__init__.py
+++ yearly_hue/__init__.py
@@ -1,15 +1,19 @@
 """Get hue."""
-from datetime import datetime
 import colorsys
+from datetime import datetime
 
 
-def _i_to_h(i: float) -> str:
+def _f_to_h(i: float) -> str:
     h = hex(int(i * 255))
     h = "00" + h[2:]
     return h[-2:]
 
 
-def get_hue(brightness: float = 1.0, saturation: float = 1.0) -> str:
+def _f_to_i(f: float) -> int:
+    return int(f * 255)
+
+
+def get_hue(brightness: float = 1.0, saturation: float = 1.0, format: str = "{hex}") -> str:
     """Return a hex representation of a hue."""
     t = datetime.today()
     hue = (int(t.strftime("%j")) - 1 + (t.hour + (t.minute + t.second / 60) / 60) / 24) / 365
@@ -17,7 +21,8 @@ def get_hue(brightness: float = 1.0, saturation: float = 1.0) -> str:
     while hue > 1:
         hue -= 1
     r, g, b = colorsys.hsv_to_rgb(hue, saturation, brightness)
-    return "#" + _i_to_h(r) + _i_to_h(g) + _i_to_h(b)
+    hex = "#" + _f_to_h(r) + _f_to_h(g) + _f_to_h(b)
+    return format.format(r=_f_to_i(r), g=_f_to_i(g), b=_f_to_i(b), hex=hex)
 
 
 if __name__ == "__main__":
--- yearly_hue/__main__.py
+++ yearly_hue/__main__.py
@@ -4,8 +4,9 @@ from argparse import ArgumentParser
 from . import get_hue
 
 ap = ArgumentParser()
+ap.add_argument("--format", default="{hex}")
 ap.add_argument("brightness", nargs='?', type=float, default=1)
 ap.add_argument("saturation", nargs='?', type=float, default=1)
 args = ap.parse_args()
 
-print(get_hue(args.brightness, args.saturation))
+print(get_hue(args.brightness, args.saturation, format=args.format))
