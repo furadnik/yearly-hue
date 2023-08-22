6c4067d2cc64909df14786161da41190a88da8d2
Filip Uradnik <filip.uradnik@nic.cz>
2023-08-22 16:03:05 +0200

    Add console script.

--- yearly_hue/__main__.py
+++ yearly_hue/__main__.py
@@ -3,10 +3,17 @@ from argparse import ArgumentParser
 
 from . import get_hue
 
-ap = ArgumentParser()
-ap.add_argument("--format", default="{hex}")
-ap.add_argument("brightness", nargs='?', type=float, default=1)
-ap.add_argument("saturation", nargs='?', type=float, default=1)
-args = ap.parse_args()
 
-print(get_hue(args.brightness, args.saturation, format=args.format))
+def main() -> None:
+    """Run the main function."""
+    ap = ArgumentParser()
+    ap.add_argument("--format", default="{hex}")
+    ap.add_argument("brightness", nargs='?', type=float, default=1)
+    ap.add_argument("saturation", nargs='?', type=float, default=1)
+    args = ap.parse_args()
+
+    print(get_hue(args.brightness, args.saturation, format=args.format))
+
+
+if __name__ == '__main__':
+    main()
